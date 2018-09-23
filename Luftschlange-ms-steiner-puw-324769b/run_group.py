0bf30294cf8f18c58e845142518b9d307e39c3b0
VitorA29 <vitor_araujo.29@outlook.com>
2018-09-23 19:56:43 -0300

    Improving execution and correcting some mistakes

--- Luftschlange-ms-steiner-puw-324769b/run_group.py
+++ Luftschlange-ms-steiner-puw-324769b/run_group.py
@@ -6,56 +6,69 @@ import json
 from pprint import pprint
 
 def main():
+    #Getting timestamp
     dts = datetime.datetime.utcnow()
     timestamp = round(time.mktime(dts.timetuple()) + dts.microsecond/1e6)
     os.system("mkdir output/" + str(timestamp))
 
+    #Getting start time and iterations
     details_report = dict()
     time_format = '%Y-%m-%d %H:%M:%S'
     details_report["start"] = datetime.datetime.now().strftime(time_format)
-    details_report["iterations"] = int(sys.argv[1])
+    details_report["iterations"] = int(sys.argv[2])
 
-    final_dictionary = dict()
-    for folder in os.listdir("instances/luidi"):
-        path = "luidi/" + folder
+    #Opening instances folder
+    report_dict = dict()
+    deviation_dict = dict()
+    elite_dict = dict()
+    for folder in os.listdir("instances/" + sys.argv[1]):
+        path = sys.argv[1] + "/" + folder
 
+        #Preparing output folder
         output_folder = str(timestamp) + "/" + folder
         os.system("mkdir output/" + output_folder)
 
+        #Getting instances files
         onlyfiles = [f for f in os.listdir("instances/" + path) if isfile(join("instances/" + path, f))]
 
-        execution_dictionary = dict()
-
+        #Executing instances
+        report = dict()
         for instance in onlyfiles:
             instance_name = instance.split(".")[0]
             if ".json" in instance:
                 continue
-            os.system("./bin/run_instance " + path + "/" + instance + " -seed 10 -maxit " + sys.argv[1] + " -mine 0 -folder " + output_folder)
+            os.system("./bin/run_instance " + path + "/" + instance + " -seed 1 -maxit " + sys.argv[2] + " -mine 0 -folder " + output_folder)
             with open("output/" + output_folder + "/" + instance_name + "/" + instance_name + ".json") as f:
                 data = json.load(f)
-                execution_dictionary[instance_name] = data
+                report[instance_name] = data
             os.system("rm output/" + output_folder + "/" + instance_name + "/" + instance_name + ".json")
 
-        with open("output/" + output_folder + "/execution.json", 'w') as f:
-            json.dump(execution_dictionary, f, indent=4, sort_keys=True)
-
-        data = dict()
-        with open("output/" + output_folder + "/execution.json") as f:
-            report = json.load(f)
-            with open("instances/" + path + "/opt.json") as fopt:
+        #Validating group
+        deviation_data = dict()
+        elite_data = dict()
+        with open("instances/" + path + "/opt.json") as fopt:
                 opt = json.load(fopt)
                 for key in report:
-                    average = sum(seed["best"] for seed in report[key].values())/len(report[key])
-                    data[key] = ((average-opt[key])/opt[key])
+                    average = sum(seed_execution["best"] for seed_execution in report[key].values())/len(report[key])
+                    deviation_data[key] = ((average-opt[key])/opt[key])
+                    elite_data[key] = [len(seed_execution["elite"]) for seed_execution in report[key].values()]
 
-        with open("output/" + output_folder + "/results.json", 'w') as f:
-            json.dump(data, f, indent=4, sort_keys=True)
-            final_dictionary[folder] = data
-        os.system("rm output/" + output_folder + "/results.json")
+        #Saving execution reports
+        deviation_dict[folder] = deviation_data
+        elite_dict[folder] = elite_data
+        report_dict[folder] = report
+            
+    #Ending execution
     details_report["stop"] = datetime.datetime.now().strftime(time_format)
-    final_dictionary["#details"] = details_report
+    deviation_dict["#details"] = details_report
+    elite_dict["#details"] = details_report
+    report_dict["#details"] = details_report
+    with open("output/" + str(timestamp) + "/deviation.json", 'w') as f:
+        json.dump(deviation_dict, f, indent=4, sort_keys=True)
+    with open("output/" + str(timestamp) + "/elite.json", 'w') as f:
+        json.dump(elite_dict, f, indent=4, sort_keys=True)
     with open("output/" + str(timestamp) + "/results.json", 'w') as f:
-        json.dump(final_dictionary, f, indent=4, sort_keys=True)
+        json.dump(report_dict, f, indent=4, sort_keys=True)
 
 if __name__ == "__main__":
     main()
