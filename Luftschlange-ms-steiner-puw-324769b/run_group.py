6506f794fa8e96f2a50ca5254c66dee2adceaa4d
VitorA29 <vitor_araujo.29@outlook.com>
2018-12-27 01:18:01 -0200

    Adicionado agrupamento de pattern por instância

--- Luftschlange-ms-steiner-puw-324769b/run_group.py
+++ Luftschlange-ms-steiner-puw-324769b/run_group.py
@@ -2,6 +2,7 @@ import os
 from os.path import isfile, join
 import datetime, sys, time, json
 from pprint import pprint
+from collections import OrderedDict
 
 def main():
     #Getting timestamp
@@ -38,11 +39,66 @@ def main():
             instance_name = instance.split(".")[0]
             if ".json" in instance:
                 continue
-            os.system("./bin/run_instance " + path + "/" + instance + " -seed " + sys.argv[3] + " -maxit " + sys.argv[2] + " -mine " + sys.argv[4] + " -folder " + output_folder)
+            max_seed = sys.argv[3]
+            data_mining = sys.argv[4]
+            os.system("./bin/run_instance " + path + "/" + instance + " -seed " + max_seed + " -maxit " + sys.argv[2] + " -mine " + data_mining + " -folder " + output_folder)
             with open("output/" + output_folder + "/" + instance_name + "/" + instance_name + ".json") as f:
                 data = json.load(f)
                 report[instance_name] = data
             os.system("rm output/" + output_folder + "/" + instance_name + "/" + instance_name + ".json")
+
+            if data_mining == '1':
+                #Building pattern instance file
+                for pattern_file_marker in ["V", "E"]:
+                    pattern_dict = dict()
+                    for seed in range (1, int(max_seed) + 1):
+                        try:
+                            with open("output/" + str(timestamp) + "/" + folder + "/" + instance_name + "/" + str(seed) + "/pattern" + pattern_file_marker + ".txt") as pattern_file:
+                                line = pattern_file.readline()
+                                while line:
+                                    line_split = line.split(";")
+                                    if(len(line_split) == 3):
+                                        # preparing new dict entry
+                                        pattern_execution_dict = dict()
+                                        pattern_execution_dict["seed"] = seed
+                                        pattern_execution_helper = line_split[2].split(" ")
+                                        pattern_execution_helper[-1] = pattern_execution_helper[-1][:-1]
+                                        if pattern_file_marker == "V":
+                                            pattern_execution_dict["pattern"] = [int(vertex) for vertex in pattern_execution_helper]
+                                        else:
+                                            pattern_execution_dict["pattern"] = pattern_execution_helper
+
+                                        if( str(line_split[1]) in pattern_dict ):
+                                            if( str(line_split[0]) in pattern_dict[str(line_split[1])] ):
+                                                #adding new entry to the array
+                                                pattern_dict[str(line_split[1])][str(line_split[0])].append(pattern_execution_dict)
+                                            else:
+                                                #adding new entry to the created array
+                                                pattern_dict[str(line_split[1])][str(line_split[0])] = []
+                                                pattern_dict[str(line_split[1])][str(line_split[0])].append(pattern_execution_dict)
+
+                                        else:
+                                            #adding new entry for first time
+                                            pattern_dict[str(line_split[1])] = dict()
+                                            pattern_dict[str(line_split[1])][str(line_split[0])] = []
+                                            pattern_dict[str(line_split[1])][str(line_split[0])].append(pattern_execution_dict)
+                                    line = pattern_file.readline()
+                        except IOError:
+                            pass
+                    #saving the instance pattern dict
+                    if len(pattern_dict) > 0:
+                        pattern_dict_json_name = "output/" + str(timestamp) + "/" + folder + "/" + instance_name + "/pattern" + pattern_file_marker + ".json"
+                        with open(pattern_dict_json_name, 'w') as f:
+                            json.dump(pattern_dict, f, indent=4, sort_keys=True)
+                        #Correcting the keys order
+                        with open(pattern_dict_json_name, 'r') as f:
+                            pattern_dict = json.load(f)
+                        reversed_pattern_dict = OrderedDict()
+                        for key, value in sorted(pattern_dict.items(), reverse=True):
+                            reversed_pattern_dict[key] = {key_: value_ for key_, value_ in sorted(value.items(), reverse=True)}
+                        with open(pattern_dict_json_name, 'w') as f:
+                            json.dump(reversed_pattern_dict, f, indent=4)
+
 
         #Validating group
         deviation_data = dict()
