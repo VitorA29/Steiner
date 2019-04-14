62d011c13cf4b96049e9b33d1c30569726251183
VitorA29 <vitor_araujo.29@outlook.com>
2019-04-14 01:21:32 +0000

    Created Analyzer V1

--- Luftschlange-ms-steiner-puw-324769b/analyze.py
+++ Luftschlange-ms-steiner-puw-324769b/analyze.py
@@ -22,29 +22,67 @@ def main():
                             best_seed = seed
                             best_elite_index = index + 1
                             best_cost = cost
-                best_solution_dict[group][instance] = set()
-                with open(used_time_stamp_folder + "/" + group + "/" + instance + "/" + best_seed + "/EliteE.txt") as elite_file:
-                    index = 0
-                    elite_edges_value = ""
-                    while index != best_elite_index:
-                        elite_edges_value = elite_file.readline()
-                        index += 1
-                    for edge_index in elite_edges_value.split(" "):
-                        if edge_index == "\n":
-                            continue
-                        best_solution_dict[group][instance].add(edge_index)
+                
+                best_solution_dict[group][instance] = dict()
+                best_solution_dict[group][instance]["edges"] = list()
+                vertex_set = set()
 
-    for root, dirs, files in os.walk(used_time_stamp_folder):
-        if root == used_time_stamp_folder:
-            continue
-        for f in files:
-            print(root, end=" - ")
-            print(f)
+                elite_match = False
+                with open(used_time_stamp_folder + "/" + group + "/" + instance + "/" + best_seed + "/EliteD.txt") as elite_file:
+                    for line in elite_file:
+                        if "Index" in line:
+                            if elite_match:
+                                break
+                            else:
+                                splited_line = line.split(" ")
+                                actual_index = splited_line[1][:-1]
+                                elite_match = best_elite_index == int(actual_index)
+                        elif elite_match and "e" in line:
+                            edge_description = line.split(" ")
+                            vertex_set.add(edge_description[1])
+                            vertex_set.add(edge_description[2])
+                            best_solution_dict[group][instance]["edges"].append("X"+edge_description[1]+"_"+edge_description[2])
+                best_solution_dict[group][instance]["vertex"] = list(vertex_set)
 
-    for group in best_solution_dict.keys():
-        for instance in best_solution_dict[group].keys():
-            print(group, end=" - ")
-            print(instance, end=" - ")
-            print(best_solution_dict[group][instance])
+                best_solution_dict[group][instance]["#viability"] = 0
+                for root, dirs, files in os.walk(used_time_stamp_folder + "/" + group + "/" + instance):
+                    if "patternE.txt" in files:
+                        seed = root.split("/")[-1]
+                        best_solution_dict[group][instance][seed] = dict()
+                        with open(root + "/patternE.txt") as pattern_file:
+                            line_index = 1
+                            pattern_process_dict = dict()
+                            for line in pattern_file:
+                                A_diff_B = set()
+                                B_diff_A = set()
+                                intercession = set()
+                                elite_edges = set(best_solution_dict[group][instance]["edges"].copy())
+                                elite_vertex = set(best_solution_dict[group][instance]["vertex"].copy())
+
+                                pattern_edges = line.split(";")[-1][:-1].split(" ")
+                                for edge in elite_edges:
+                                    if not edge in pattern_edges:
+                                        A_diff_B.add(edge)
+                                    else:
+                                        intercession.add(edge)
+
+                                for edge in pattern_edges:
+                                    if not edge in elite_edges:
+                                        B_diff_A.add(edge)
+                                        pattern_vertex = edge[1:].split("_")
+                                        if pattern_vertex[0] in elite_vertex and pattern_vertex[1] in elite_vertex:
+                                            best_solution_dict[group][instance]["#viability"] += 1
+                                pattern_process_dict[line_index] = dict()
+                                pattern_process_dict[line_index]["#A-B"] = list(A_diff_B)
+                                pattern_process_dict[line_index]["#B-A"] = list(B_diff_A)
+                                pattern_process_dict[line_index]["#intercession"] = list(intercession)
+                                line_index += 1
+
+                            best_solution_dict[group][instance][seed] = pattern_process_dict
+                        print("done " + seed)
+                print("done " + instance)
+
+    with open(used_time_stamp_folder + "/analyze.json", 'w') as f:
+        json.dump(best_solution_dict, f, indent=4, sort_keys=True)
 
 main()
